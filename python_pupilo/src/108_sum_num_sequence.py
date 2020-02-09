165f955cdf1cced65fabc23f3dc3803d71048a48
Camila <camila.bodack@assetify.com.br>
2020-02-09 14:13:21 -0300

    solve exercise 108

---
+++ python_pupilo/src/108_sum_num_sequence.py
@@ -0,0 +1,14 @@
+num = int(input("Número final da sequencia:"))
+a = 1
+b = 1
+h = 1
+soma_a = 0
+soma_b = 0
+
+while a < num and b < num-1:
+    a += 1
+    soma_a = soma_a + a
+    b += 2
+    soma_b = soma_b + b
+    print(a, "/", b)
+print("Soma termos:", "A:", soma_a + h, "B:", soma_b + h)
