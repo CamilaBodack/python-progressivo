514e02d3242b59f2acd77f3d69f4758b132c9bb7
CamilaBodack <camila.bodack@gmail.com>
2020-07-30 19:29:13 -0300

    exercicio de pilha

--- estruturas_de_dados/03_pilha.py
+++ estruturas_de_dados/03_pilha.py
@@ -2,9 +2,24 @@
 Pilhas: o primeiro item que entra é o último a sair da estrutura.
 '''
 
+
 class Pilha:
     def __init__(self):
         self.pilha = []
 
-    def push(self, dados):
-        self.pilha.append(dados)
+    def insere(self, dados):
+        return self.pilha.append(dados)
+
+    def remove(self):
+        return self.pilha.pop()
+
+    def topo(self):
+        return self.pilha[-1]
+
+
+nova_pilha = Pilha()
+nova_pilha.insere(1)
+nova_pilha.insere(2)
+nova_pilha.insere(3)
+nova_pilha.remove()
+print(nova_pilha.topo())
