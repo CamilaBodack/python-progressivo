574724cac97381bdc1ecde19165d2db12f5bd03c
CamilaBodack <camila.bodack@gmail.com>
2020-08-03 14:35:00 -0300

    adiciona if para verificar faixa de 11 a 19

--- numero_extenso/numero_por_extenso.py
+++ numero_extenso/numero_por_extenso.py
@@ -1,27 +1,4 @@
 
-'''
-Conversão de números para palavras Desafio
-
-Na linguagem de sua preferência, crie um servidor HTTP que, para cada
-requisiçãoGET, retorne um JSON cuja chave extenso seja a versão por extenso
-do númerointeiro enviado no path. Os números podem estar no intervalo
-[-99999, 99999].
-Exemplos: λ curl http://localhost:3000/1 { "extenso": "um" }
-λ curl http://localhost:3000/-1042 { "extenso": "menos mil e quarenta e dois" }
-λ curl http://localhost:3000/94587 { "extenso": "noventa e quatro mil e
-quinhentos e oitenta e sete" } Nos mande o link do repositório no GitHub com o
-código em até sete dias. Se você abrir um Pull Request (p.ex. do seu branch de
-desenvolvimento para o master) nós faremos o review e você terá a chance de
-corrigir os erros para uma segunda avaliação. Não esqueça do README.md com
-as instruções para rodar o servidor! Não esqueça dos "e"s separando milhares,
-centenas e dezenas (vide exemplo): "noventa e quatro mil e quinhentos e oitenta
-e sete". Esse não é o padrão da norma culta da língua portuguesa,
-e isso é intencional. É esperado que você implemente o algoritmo de tradução.
-Mesmo que não esteja com a lógica completa, nos envie o que conseguiu fazer até
-o momento.
-'''
-
-
 # 1. quatros listas
 # 2. if para dividir as duas sequencias de dezenas
 # 3. adicionar "e" a cada vez que avança uma lista
@@ -37,7 +14,7 @@ numero = int(input("insira o número"))
 
 # servirá para verificar se a dezena esta entre 11 e 19
 
-verifica_dezena = int(numero % 100)
+faixa_dezena = int(numero % 100)
 
 unidade = int(numero % 10)
 numero = (numero - unidade)/10
@@ -65,17 +42,17 @@ def verifica_centena(centena):
     return centenas[centena]
 
 
-def verifica_faixa_dezena(verifica_dezena):
-    # verificar posicao entre 11 e 19
-    if (verifica_dezena > 10 and verifica_dezena < 20):
+def verifica_faixa_dezena(faixa_dezena, dezena):
+    if (faixa_dezena > 10 and faixa_dezena < 20):
+        posicao = int(faixa_dezena % 10)
         dezena_inicial = ["onze", "doze", "treze", "quatorze", "quinze",
                           "dezesseis", "desessete", "dezoito", "dezenove",
                           None]
-        return dezena_inicial[verifica_dezena]
+        return dezena_inicial[posicao]
     else:
-        dezena = ["dez", "vinte", "trinta", "quarenta", "cinquenta",
-                  "sessenta", "setenta", "oitenta", "noventa", None]
-        return dezena[verifica_dezena]
+        dezenas = ["dez", "vinte", "trinta", "quarenta", "cinquenta",
+                   "sessenta", "setenta", "oitenta", "noventa", None]
+        return dezenas[dezena]
 
 
 def verifica_unidade(unidade):
@@ -84,5 +61,7 @@ def verifica_unidade(unidade):
     return unidades[unidade]
 
 
-verifica_milhar(milhar), verifica_centena(centena), verifica_dezena(dezena),
-verifica_unidade(unidade), verifica_faixa_dezena(verifica_dezena)
+print(verifica_milhar(milhar))
+print(verifica_centena(centena))
+print(verifica_faixa_dezena(faixa_dezena, dezena))
+print(verifica_unidade(unidade))
